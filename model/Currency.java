d480391a1b710a31e4778cc1f03971ce2ddcb1a0
Oliver <138564347+oliverguerreiro@users.noreply.github.com>
2023-07-06 11:50:21 +0200

    added model classes

---
+++ model/Currency.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Currency {
+    private String currencyCode;
+    private String lastUpdatedDate;
+    private float price;
+
+
+}
---
+++ model/User.java
@@ -0,0 +1,9 @@
+package model;
+
+public class User {
+    private String firstName;
+    private String lastName;
+    private String DOB;
+    private String gender;
+    private String country;
+}
