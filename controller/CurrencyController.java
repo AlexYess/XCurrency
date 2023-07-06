50450276e28160c3c9717314cc46ae830a32f64d
Oliver <138564347+oliverguerreiro@users.noreply.github.com>
2023-07-06 14:15:07 +0200

    Added currency controller

--- controller/CurrencyController.java
+++ controller/CurrencyController.java
@@ -1,4 +1,31 @@
 package controller;
 
+import model.Currency;
+import service.CurrencyService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 public class CurrencyController {
+    private final CurrencyService currencyService;
+    public CurrencyController(CurrencyService currencyService) {
+        this.currencyService = currencyService;
+    }
+    public List<Currency> GetAllCurrencies() {
+        return currencyService.GetAllCurrencies();
+    }
+    public void insertCurrency(Currency newCurrency) {
+        try {
+            currencyService.insertCurrency(newCurrency);
+        } catch (IllegalArgumentException e) {
+            throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY);
+        }
+    }
+    public Optional<Currency> GetCurrencyCode(String code) {
+        if (currencyService.GetCurrencyCode(code).isPresent()) {
+            throw new IllegalArgumentException("Currency already present");
+        }
+        return Optional.of(currencyService.GetCurrencyCode(code).get());
+    }
 }
--- service/CurrencyService.java
+++ service/CurrencyService.java
@@ -6,6 +6,16 @@ import java.util.*;
 
 public class CurrencyService {
     private List<Currency> currencyList = new ArrayList<>();
+
+    public List<Currency> GetAllCurrencies() {
+        return currencyList;
+    }
+    public void insertCurrency(Currency newCurrency) {
+        if (GetCurrencyCode(newCurrency.getCurrencyCode()).isPresent()) {
+            throw new IllegalArgumentException("Currency already present");
+        }
+        currencyList.add(newCurrency);
+    }
     public Optional<Currency> GetCurrencyCode(String code) {
         for (Currency currency : currencyList) {
             if (currency.getCurrencyCode().equals(code)) {
