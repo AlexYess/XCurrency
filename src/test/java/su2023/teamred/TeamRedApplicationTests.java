4dddd1a5a56b54dcccff04650865558e7e20f6af
Tinghong Jin <jin.t.ac@m.titech.ac.jp>
2023-07-06 14:26:20 +0200

    boot from spring.io, changed file structure

---
+++ src/test/java/su2023/teamred/TeamRedApplicationTests.java
@@ -0,0 +1,13 @@
+package su2023.teamred;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamRedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
