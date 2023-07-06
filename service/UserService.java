f16f2bd1a4c2d71bf86b9bf0ff5919f10244b066
Tinghong Jin <jin.t.ac@m.titech.ac.jp>
2023-07-06 12:26:14 +0200

    add UserService.java

---
+++ service/UserService.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+public class UserService {
+    private List<User> users = new ArrayList<>();
+
+    public List<User> getAllUsers() {
+        return users;
+    }
+
+    public void insertUser(User newUser) {
+        Optional<User> userWithGivenId = getUser(newUser.getUserID());
+        if(userWithGivenId.isPresent()) {
+            throw new IllegalArgumentException("Id already present");
+        }
+        users.add(newUser);
+    }
+
+    public Optional<User> GetUser(Long userID) {
+        for (User user : users) {
+            if (userID.equals(user.getUserID())) {
+                return Optional.of(user);
+            }
+        }
+        return Optional.empty();
+    }
+
+}
