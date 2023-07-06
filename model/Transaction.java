fa9faccaff20f14760d8b389a8e5950e6360bbae
Oliver <138564347+oliverguerreiro@users.noreply.github.com>
2023-07-06 12:04:07 +0200

    added constructors

---
+++ model/Transaction.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Transaction {
+    private Long transactionID;
+    private Long sellerID;
+    private Long buyerID;
+    private String currencyCodeFrom;
+    private String currencyCodeTo;
+    private float rate;
+    private float amount;
+    private float price;
+    private String expiryDate;
+    private boolean isApproved;
+
+    public Transaction(Long transactionID, Long sellerID, Long buyerID, String currencyCodeFrom, String currencyCodeTo, float rate, float amount, String expiryDate, boolean isApproved) {
+        this.transactionID = transactionID;
+        this.sellerID = sellerID;
+        this.buyerID = buyerID;
+        this.currencyCodeFrom = currencyCodeFrom;
+        this.currencyCodeTo = currencyCodeTo;
+        this.rate = rate;
+        this.amount = amount;
+        this.price = rate * amount;
+        this.expiryDate = expiryDate;
+        this.isApproved = isApproved;
+    }
+}
--- model/User.java
+++ model/User.java
@@ -1,9 +1,28 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.List;
+import model.Transaction;
+
 public class User {
+    private Long userID;
     private String firstName;
     private String lastName;
+    private String email;
     private String DOB;
     private String gender;
     private String country;
+    private String preferredCurrency;
+    private List<Transaction> transactionHistory = new ArrayList<>();
+
+    public User(Long userID, String firstName, String lastName, String email, String DOB, String gender, String country, String preferredCurrency) {
+        this.userID = userID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.DOB = DOB;
+        this.gender = gender;
+        this.country = country;
+        this.preferredCurrency = preferredCurrency;
+    }
 }
