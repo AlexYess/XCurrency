57b0c87d8d87015a9ebbf7749cf5ea35b30a9bde
Oliver <138564347+oliverguerreiro@users.noreply.github.com>
2023-07-07 12:26:57 +0200

    updated currencyService

--- src/main/java/com/example/CurrencyExchange/service/CurrencyService.java
+++ src/main/java/com/example/CurrencyExchange/service/CurrencyService.java
@@ -1,13 +1,22 @@
 package com.example.CurrencyExchange.service;
 
 import com.example.CurrencyExchange.model.Currency;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @Service
 public class CurrencyService {
+    private static final String[] baseUrlsArr = new String[]{"https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/", "https://raw.githubusercontent.com/fawazahmed0/currency-api/1/"};
+    private static final String[] suffixArr = new String[]{".min.json", ".json"};
     private final List<Currency> currencyList = new ArrayList<>();
 
     public List<Currency> getAllCurrencies() {
