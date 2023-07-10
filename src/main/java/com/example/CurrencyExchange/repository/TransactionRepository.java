18d06c573511e31154141c8155016b2a747801b3
AlexYess <aalexesetov2541@gmail.com>
2023-07-10 11:14:21 +0200

    some more shit

--- src/main/java/com/example/CurrencyExchange/repository/TransactionRepository.java
+++ src/main/java/com/example/CurrencyExchange/repository/TransactionRepository.java
@@ -1,6 +1,12 @@
 package com.example.CurrencyExchange.repository;
 
-public class TransactionRepository {
+import com.example.CurrencyExchange.model.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-    public interface
+import java.util.List;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    List<Transaction> FindByUserID(Long ID);
 }
